75926043f2cb22168e7c0d99af9249b2b5b5dde6
shorifulbd1st <shorifulbd1st@gmail.com>
2025-03-13 05:21:45 +0600

    create foldre structure and readme.md file

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1>This is home page</h1>
+      <h1 className='text-5xl text-red-500'>This is home page</h1>
     </div>
   )
 }
