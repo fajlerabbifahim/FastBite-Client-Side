1fc41ad5a9bba902e55a9499cfc831dd775feed8
Yourfaahim <fahimqw1@gmail.com.com>
2025-03-12 10:43:41 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="text-6xl text-red-800">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
